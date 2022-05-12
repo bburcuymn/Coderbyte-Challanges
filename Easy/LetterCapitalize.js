895f370a6c48edaeada31ea12a8899914513c94e
Burcu Yaman <59296593+bburcuymn@users.noreply.github.com>
2022-05-12 16:28:15 +0300

    Add files via upload

---
+++ Easy/LetterCapitalize.js
@@ -0,0 +1,25 @@
+function LetterCapitalize(str) {
+    //code goes here
+
+let arr = str.split(" "); // Verilen ifadeyi kelimelere ayırarak array oluştur.
+
+for(let i=0; i<arr.length; i++){ // Sırayla dizideki her elemana (kelimeye) bak.
+
+    let word = "";
+
+    for(let j =0; j<arr[i].length; j++){ // Seçilen kelimenin harflerine bak.
+
+        if(j===0){
+            word += arr[i][j].toUpperCase(); // Eğer ilk harfse büyük harfe çevir.   
+        }
+        else{
+            word+= arr[i][j]; // ilk harf değilse değiştirmeden ekle.
+        }
+    }
+    arr[i]=word;
+}
+return arr.join(" ");
+
+}
+
+console.log(LetterCapitalize(readline()));
